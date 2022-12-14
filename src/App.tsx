d00ecd279d082e2678e38a1cd7f61b313793283e
Mariia Riabikova <mariiar@mobiquityinc.com>
2022-12-14 14:02:36 +0100

    [#1-fix]: fixed git issues, delete files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Footer} from './Layout/Footer/Footer'
+import {Header} from "./Layout/Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      {/*<Main/>*/}
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
