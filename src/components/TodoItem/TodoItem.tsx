9e3e2d5d58c4264349d83280d06721627dd0bfe6
Mariia Riabikova <mariiar@mobiquityinc.com>
2022-12-19 12:22:46 +0100

    [#8-fix]: Oleksandr: fixed editing tasks

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,31 +1,33 @@
 import React, { useState } from "react";
 import { TodoI } from "../../types";
+
 interface TodoItemProps {
-    todo: TodoI;
-    setTodo: (todo: TodoI) => void;
-  removeItem: (id: number)=> void
+  todo: TodoI;
+  todos: TodoI[];
+  setTodos: any;
 }
+
 export const TodoItem: React.FC<TodoItemProps> = (props) => {
   const [isEditingID, setIsEditingID] = useState<number>(0);
-
   const [editingText, setEditingText] = useState<string>("");
 
   const toggleTodo = (id: number) => {
-    //   const togglingToDo =
-    //       props.todos.map((todo) => {
-    //       if (todo.id !== id) return todo;
-    //       return {
-    //           ...todo,
-    //           complete: !todo.complete,
-    //       };
-    //   })
-    // props.setTodos(togglingToDo);
+    props.setTodos(
+      props.todos.map((todo) => {
+        if (todo.id !== id) return todo;
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      })
+    );
   };
 
   const removeTodo = (id: number) => {
     props.setTodos(props.todos.filter((todo) => todo.id !== id));
     //setAllTodos(allTodos - 1);
   };
+
   const editTodo = (id: number, todoItemText: string) => {
     props.setTodos(
       props.todos.map((todo) => {
@@ -37,14 +39,17 @@ export const TodoItem: React.FC<TodoItemProps> = (props) => {
       })
     );
   };
+
   const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEditingText(event.target.value);
   };
+
   const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     editTodo(props.todo.id, editingText);
     setIsEditingID(0);
   };
+
   return (
     <li>
       {isEditingID === props.todo.id ? (
@@ -57,14 +62,11 @@ export const TodoItem: React.FC<TodoItemProps> = (props) => {
             required
           />
         </form>
-
       ) : (
         <span>{props.todo.name}</span>
       )}
       <input type="checkbox" onClick={() => toggleTodo(props.todo.id)}></input>
-
       <button onClick={() => removeTodo(props.todo.id)}>x</button>
-
       <button onClick={() => setIsEditingID(props.todo.id)}>u</button>
     </li>
   );
