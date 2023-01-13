7b2b22168d2b965f673e52c859f9b490daf8f1c4
Oleksandr <trafik.reno1@gmail.com>
2023-01-13 16:35:36 +0100

    [#16]: Removed the wrong part of code

--- src/components/ModalWindow/DeleteConfiramtion/DeleteConfiramtion.styles.ts
+++ src/components/ModalWindow/DeleteConfiramtion/DeleteConfiramtion.styles.ts
@@ -1,59 +1,5 @@
 import styled from "styled-components";
 
-// .title {
-//   font-size: 2.5rem;
-//   margin-bottom: 2rem;
-//   padding-bottom: 2rem;
-//   border-bottom: 0.2rem solid rgba(35, 33, 40, 0.15);
-// }
-
-// /* MESSAGE */
-// .message {
-//   text-align: center;
-//   background-color: #e9aaaa;
-//   color: #501111;
-//   margin-bottom: 2rem;
-//   padding: 1rem;
-//   border-radius: 1rem;
-//   font-size: 1.6rem;
-// }
-
-// /* BUTTONS */
-// .btnContainer {
-//   display: flex;
-//   justify-content: flex-end;
-//   padding-top: 2rem;
-//   border-top: 0.2rem solid rgba(35, 33, 40, 0.15);
-// }
-
-// .btn {
-//   font-size: 1.6rem;
-//   border: none;
-//   background-color: #e9ecef;
-//   padding: 1rem;
-//   border-radius: 10rem;
-//   transition: 0.2s;
-// }
-
-// .btn:hover {
-//   cursor: pointer;
-// }
-
-// .cancelBtn:hover {
-//   outline: 0.2rem solid rgb(35, 33, 40);
-// }
-
-// .confirmBtn {
-//   margin-left: 3.2rem;
-//   background-color: #ff6b6b;
-//   color: #501111;
-// }
-
-// .confirmBtn:hover {
-//   color: #e9ecef;
-//   outline: 0.2rem solid rgb(35, 33, 40);
-// }
-
 export const Title = styled.p`
    {
     font-size: 2.5rem;
